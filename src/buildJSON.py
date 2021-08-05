513314d3c690a4771ac89475a0b4ebeeebe07c20
rali-udem <critter@iro.umontreal.ca>
2021-08-05 10:46:22 -0400

    Quick initial refactoring.

--- src/buildJSON.py
+++ src/buildJSON.py
@@ -3,7 +3,7 @@ from ppJson import ppJson
 
 import glob,os,json,re, sys
 
-baseDir="/Users/lapalme/Documents/GitHub/IVADO-BuildData/testDir"
+baseDir=os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../testDir'))
 
 ## get text of the bulletin for the region name and build a structure with the text,
 ## the key is the first line with the region name without the the ending "."
@@ -44,26 +44,28 @@ def combineBulletin(jsonFN,obj):
         sys.exit(1)
     bulletinName=f'{m.group("baseDir")}Bulletins{m.group("yearProv")}{m.group("fileName")}'
     # print(bulletinName)
-    en=getBulletins(bulletinName+".e")
-    fr=getBulletins(bulletinName+".f")
-    
-    header=obj[0]["entete"]
-    for iRegion in range(1,len(obj)):
-        res={"header":header}
-        regionObj=obj[iRegion]
-        regionNos=regionObj["regions"][0]
-        res["names-en"]= [getRegionName(regionNo,"en") for regionNo in regionNos]
-        res["names-fr"]= [getRegionName(regionNo,"fr") for regionNo in regionNos]
-        for key,val in regionObj.items():
+    save_bulletin_texts(bulletinName, obj, f'{m.group("baseDir")}JSON{m.group("yearProv")}{m.group("fileName")[9:]}')
+
+
+def save_bulletin_texts(bulletin_name_root, meteo_code, region_json_dirname):
+    en = getBulletins(bulletin_name_root + ".e")
+    fr = getBulletins(bulletin_name_root + ".f")
+    header = meteo_code[0]["entete"]
+    for iRegion in range(1, len(meteo_code)):
+        res = {"header": header}
+        regionObj = meteo_code[iRegion]
+        regionNos = regionObj["regions"][0]
+        res["names-en"] = [getRegionName(regionNo, "en") for regionNo in regionNos]
+        res["names-fr"] = [getRegionName(regionNo, "fr") for regionNo in regionNos]
+        for key, val in regionObj.items():
             if key not in ignoredKeys:
-                res[key]=val
-        res["en"]=getBulletin(en,res["names-en"][0])
-        res["fr"]=getBulletin(fr,res["names-fr"][0])
-        regionJSONdir=f'{m.group("baseDir")}JSON{m.group("yearProv")}{m.group("fileName")[9:]}'
-        if not os.path.exists(regionJSONdir):
-            os.makedirs(regionJSONdir)
-        regionJSONFN=f"{regionJSONdir}/{regionNos[0]}.json"
-        ppJson(open(regionJSONFN,"w",encoding="utf-8"),res,0,False)
+                res[key] = val
+        res["en"] = getBulletin(en, res["names-en"][0])
+        res["fr"] = getBulletin(fr, res["names-fr"][0])
+        if not os.path.exists(region_json_dirname):
+            os.makedirs(region_json_dirname)
+        regionJSONFN = f"{region_json_dirname}/{regionNos[0]}.json"
+        ppJson(open(regionJSONFN, "w", encoding="utf-8"), res, 0, False)
 
 
 ## check parsing of a single file
