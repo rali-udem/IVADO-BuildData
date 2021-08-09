1a3eb27099074b7f7ff06ffb8bdac1d26850ce48
rali-udem <critter@iro.umontreal.ca>
2021-08-09 11:27:05 -0400

    Added corpus collection.

--- src/make_stats.py
+++ src/make_stats.py
@@ -5,13 +5,15 @@ import sys
 from tqdm import tqdm
 
 
-def check_bulletin(cur_bulletin: dict, cur_stats: dict):
+def check_bulletin(cur_bulletin: dict, cur_stats: dict, textes: dict):
     for k, v in cur_bulletin.items():
         cur_stats[k] = cur_stats.get(k, 0) + 1
 
     nb_regions_en = len(cur_bulletin['names-en'])
     assert nb_regions_en == len(cur_bulletin['names-fr']), "Inconsistent nb of regions"
     cur_stats['nb_regions'][nb_regions_en] = stats['nb_regions'].get(nb_regions_en, 0) + 1
+    textes['en'].append(' '.join(cur_bulletin['en'].split()) + '\n')
+    textes['fr'].append(' '.join(cur_bulletin['fr'].split()) + '\n')
 
     # names_en = ', '.join(cur_bulletin['names-en'])
     # en_text = cur_bulletin['en']
@@ -20,8 +22,8 @@ def check_bulletin(cur_bulletin: dict, cur_stats: dict):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: prog input.jsonl", file=sys.stderr)
+    if len(sys.argv) != 4:
+        print("Usage: prog input.jsonl output.en output.fr", file=sys.stderr)
         sys.exit(1)
 
     input_filename = sys.argv[1]
@@ -37,3 +39,7 @@ if __name__ == '__main__':
 
     print(f"Found {nb_bulletins} bulletins.")
     pprint(stats)
+
+    for corpus, filename in zip([textes['en'], textes['fr']], sys.argv[2:]):
+        with open(filename, 'wt', encoding='utf-8') as fout:
+            fout.writelines(corpus)
